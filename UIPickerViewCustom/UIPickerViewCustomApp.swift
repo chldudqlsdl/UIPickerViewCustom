47c4fcb038b0c1bfe4c22562e4819da2e180bdef
YoungBin Choi <chldudqlsdl@naver.com>
2023-07-13 23:16:09 +0900

    Initial Commit

---
+++ UIPickerViewCustom/UIPickerViewCustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPickerViewCustomApp.swift
+//  UIPickerViewCustom
+//
+//  Created by Youngbin Choi on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct UIPickerViewCustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
